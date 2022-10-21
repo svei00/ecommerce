2f9941cea1199a19927644633b3ed911c8bea702
svei00 <79055634+svei00@users.noreply.github.com>
2022-10-20 19:52:35 -0700

    Adding product and category on the db

--- croquetas/schemas/category.js
+++ croquetas/schemas/category.js
@@ -1,7 +1,10 @@
+import { BiCategory } from 'react-icons/bi'
+
 export default {
   name: 'category',
   title: 'Category',
   type: 'document',
+  icon: BiCategory,
   fields: [
     {
       name: 'title',
@@ -14,24 +17,46 @@ export default {
       type: 'slug',
       options: {
         source: 'title',
-        maxLength: 96,
+        maxLenght: 96,
       },
     },
-    {
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    },
-    {
-      name: 'parents',
-      title: 'Parent categories',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: [{type: 'category'}],
-        },
-      ],
-    },
   ],
-}
+};
+
+// export default {
+//   name: 'category',
+//   title: 'Category',
+//   type: 'document',
+//   fields: [
+//     {
+//       name: 'title',
+//       title: 'Title',
+//       type: 'string',
+//     },
+//     {
+//       name: 'slug',
+//       title: 'Slug',
+//       type: 'slug',
+//       options: {
+//         source: 'title',
+//         maxLength: 96,
+//       },
+//     },
+//     {
+//       name: 'description',
+//       title: 'Description',
+//       type: 'text',
+//     },
+//     {
+//       name: 'parents',
+//       title: 'Parent categories',
+//       type: 'array',
+//       of: [
+//         {
+//           type: 'reference',
+//           to: [{type: 'category'}],
+//         },
+//       ],
+//     },
+//   ],
+// }
--- croquetas/schemas/product.js
+++ croquetas/schemas/product.js
@@ -1,7 +1,10 @@
+import { MdPets } from 'react-icons/md'
+
 export default {
   name: 'product',
   title: 'Product',
   type: 'document',
+  icon: MdPets,
   fields: [
     {
       name: 'title',
@@ -14,72 +17,120 @@ export default {
       type: 'slug',
       options: {
         source: 'title',
-        maxLength: 96,
+        maxLenght: 96,
       },
     },
-    {
-      title: 'Default variant',
-      name: 'defaultProductVariant',
-      type: 'productVariant',
-    },
-    {
-      title: 'Variants',
-      name: 'variants',
-      type: 'array',
-      of: [
-        {
-          title: 'Variant',
-          type: 'productVariant',
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'array',
+        of: [{ type: 'image' }],
+        options: {
+          hotspot: true,
         },
-      ],
-    },
-    {
-      title: 'Tags',
-      name: 'tags',
-      type: 'array',
-      of: [
-        {
-          type: 'string',
-        },
-      ],
-      options: {
-        layout: 'tags',
       },
-    },
-    {
-      name: 'vendor',
-      title: 'Vendor',
-      type: 'reference',
-      to: {type: 'vendor'},
-    },
-    {
-      name: 'blurb',
-      title: 'Blurb',
-      type: 'localeString',
-    },
-    {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: {type: 'category'},
-        },
-      ],
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'localeBlockContent',
-    },
-  ],
+      {
+        name: 'category',
+        title: 'Category',
+        type: 'reference',
+        to: [{ type: 'category' }]
+      },
 
-  preview: {
-    select: {
-      title: 'title',
-      manufactor: 'manufactor.title',
-      media: 'defaultProductVariant.images[0]',
-    },
-  },
-}
+      {
+        name: 'price',
+        title: 'Price',
+        type: 'number',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'blockContent',
+      },
+  ],
+};
+
+// export default {
+//   name: 'product',
+//   title: 'Product',
+//   type: 'document',
+//   fields: [
+//     {
+//       name: 'title',
+//       title: 'Title',
+//       type: 'string',
+//     },
+//     {
+//       name: 'slug',
+//       title: 'Slug',
+//       type: 'slug',
+//       options: {
+//         source: 'title',
+//         maxLength: 96,
+//       },
+//     },
+//     {
+//       title: 'Default variant',
+//       name: 'defaultProductVariant',
+//       type: 'productVariant',
+//     },
+//     {
+//       title: 'Variants',
+//       name: 'variants',
+//       type: 'array',
+//       of: [
+//         {
+//           title: 'Variant',
+//           type: 'productVariant',
+//         },
+//       ],
+//     },
+//     {
+//       title: 'Tags',
+//       name: 'tags',
+//       type: 'array',
+//       of: [
+//         {
+//           type: 'string',
+//         },
+//       ],
+//       options: {
+//         layout: 'tags',
+//       },
+//     },
+//     {
+//       name: 'vendor',
+//       title: 'Vendor',
+//       type: 'reference',
+//       to: {type: 'vendor'},
+//     },
+//     {
+//       name: 'blurb',
+//       title: 'Blurb',
+//       type: 'localeString',
+//     },
+//     {
+//       name: 'categories',
+//       title: 'Categories',
+//       type: 'array',
+//       of: [
+//         {
+//           type: 'reference',
+//           to: {type: 'category'},
+//         },
+//       ],
+//     },
+//     {
+//       name: 'body',
+//       title: 'Body',
+//       type: 'localeBlockContent',
+//     },
+//   ],
+
+//   preview: {
+//     select: {
+//       title: 'title',
+//       manufactor: 'manufactor.title',
+//       media: 'defaultProductVariant.images[0]',
+//     },
+//   },
+// }
