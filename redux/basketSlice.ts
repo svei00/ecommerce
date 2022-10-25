e6e1f16d3ec14c28f2824fc0b5080017fc2bf415
svei00 <79055634+svei00@users.noreply.github.com>
2022-10-24 20:59:09 -0700

    Added basketSlice Redux code

--- redux/basketSlice.ts
+++ redux/basketSlice.ts
@@ -1,19 +1,52 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction } from '@reduxjs/toolkit';
 
-export interface CounterState {
-    value: number;
+export interface BasketState {
+    items: Product[];
 }
 
-const initialState: CounterState = {
-    value: 0,
+// This is like useState un React
+/* const [items, setItems] = useState([]); */
+
+const initialState: BasketState = {
+    items: [],
 };
 
-export const counterSlice = createSlice({
-    name: 'counter',
+export const basketSlice = createSlice({
+    name: 'basket',
     initialState,
     reducers: {
-        increment: (state) => {
-            
-        }
-    }
-}) 
+        addToBasket: (state: BasketState, action: PayloadAction<Product>) => {
+            // Redux toolkit allow us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the inner library,
+            // which detects changes to a "draft state" and produces a brand new
+            // inmutable state based off those changes.
+            state.items = [...state.items, action.payload]; 
+        },
+        removeFromBasket: (
+            state: BasketState,
+            action: PayloadAction<{ id: string }>
+        ) => {
+            const index = state.items.findIndex(
+                (item: Product) => item._id === action.payload.id
+            );
+
+            let newBasket = [...state.items];
+
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.log(
+                    `Can't remove product  (id: ${action.payload.id}) as its not in basket!`
+                );
+            }
+
+            state.items = newBasket;
+        },
+    },
+}); 
+
+// Action creators are generated for each case reducer functions
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+export default basketSlice.reducer;
