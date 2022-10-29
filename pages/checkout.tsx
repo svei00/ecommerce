a2ec4d344ffd695d3b3a05ff577e491bca72ab22
svei00 <79055634+svei00@users.noreply.github.com>
2022-10-28 20:14:37 -0700

    Finishing the checkout page and starting the stripe checkiut

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -7,6 +7,10 @@ import CheckoutProducts from '../components/CheckoutProducts';
 import Header from '../components/Header'
 import { selectBasketItems, selectBasketTotal } from '../redux/basketSlice';
 import Currency from 'react-currency-format';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import Stripe from 'stripe';
+import { fetchPostJSON } from '../assets/js/api-helpers';
+import getStripe from '../assets/js/get-Stripejs';
 
 function Checkout() {
 
@@ -17,6 +21,8 @@ function Checkout() {
         {} as { [key: string]: Product[] }
     );
 
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         const groupedItems = items.reduce((results, item) => {
             (results[item._id] = results[item._id] || []).push(item);
@@ -25,6 +31,26 @@ function Checkout() {
 
          setGroupedItemsInBasket(groupedItems);
     }, [items]);
+
+    const createCheckoutSession = async () => {
+        setLoading(true);
+
+        const checkoutSession: Stripe.Checkout.Session = await fetchPostJSON(
+            '/api/checkout_sessions', 
+            {
+                items:items,
+            }
+        );
+
+        // Internal Server Error
+        if ((checkoutSession as any).statusCode === 500) {
+            console.error((checkoutSession as any).message);
+            return;
+        }
+
+        // Redirect to checkout
+        const stripe = await getStripe();
+    };
 
   return (
     <div className='min-h-screen overflow-hidden bg-[#fff]'>
@@ -53,12 +79,72 @@ function Checkout() {
                             <CheckoutProducts key={key} items={items} id={key} />
                         ))}
 
-                        <div>
-                            <div>
-                                <div>
-                                    <div>
+                        <div className='my-12 mt-6 ml-auto max-w-3xl'>
+                            <div className='divide-y divide-gray-300'>
+                                <div className='p4'>
+                                    <div className='flex justify-between'>
                                         <p>Subtotal</p>
                                         <p><Currency value={basketTotal} displayType={'text'} decimalSeparator="." decimalScale={2} thousandSeparator={true}  fixedDecimalScale={true} prefix={'$'}/></p>
+                                    </div>
+                                    <div className='flex justify-between'>
+                                        <p>Shipping</p>
+                                        <p>Free</p>
+                                    </div>
+                                    <div className='flex justify-between'>
+                                        <div className='flex flex-col gap-x-1 lg:flex-row'>
+                                            Estimated Tax for:{" "}
+                                            <p className='flex cursor-pointer items-end text-blue-500 hover:text-red-500 hover:font-semibold'>
+                                                Enter ZIP Code
+                                                <ChevronDownIcon className='h-6 w-6'/>
+                                            </p>
+                                        </div>
+                                        <p>$ -</p>
+                                    </div>
+                                </div>
+
+                                <div className='flex justify-between pt-4 text-xl font-semibold'>
+                                    <h4>Total</h4>
+                                    <h4>
+                                        <Currency value={basketTotal} displayType={'text'} decimalSeparator="." decimalScale={2} thousandSeparator={true}  fixedDecimalScale={true} prefix={'$'}/>
+                                    </h4>
+                                </div>
+                            </div>
+
+                            <div className='my-14 space-y-4'>
+                                <h4 className='text-xl font-semibold'>
+                                    How would you like to check out?
+                                </h4>
+                                <div className='flex flex-col gap-4 md:flex-row'>
+                                    <div className='order-2 flex flex-1 flex-col items-center rounded-xl bg-gray-200 p-8 py-12 text-center'>
+                                        <h4 className='mb-4 flex flex-col text-xl font-semibold'>
+                                            <span>Pay Monthly</span>
+                                            <span>With Credit Card</span>
+                                            <span>
+                                            <Currency value={basketTotal/12} displayType={'text'} decimalSeparator="." decimalScale={2} thousandSeparator={true}  fixedDecimalScale={true} prefix={'$'}/>
+                                                /mo at 0% APR <sup className='top-1'>0</sup>
+                                            </span>
+                                        </h4>
+                                        <Button title='Check out with Creadit Card Monthly Installments'/>
+                                        <p className='mt-2 max-w-[240px] text-[13px]'>
+                                            $ 0.00 due today, which includes applicable full-price
+                                            items, down payments, shipping and taxes.
+                                        </p>   
+                                    </div>
+
+                                    <div className='flex flex-1 flex-col items-center space-y-8 rounded-xl bg-gray-200 p-8 py-12 md:order-2'>
+                                        <h4 className='mb-4 flex flex-col text-xl font-semibold'>
+                                            Pay in Full.
+                                            <span>
+                                            <Currency value={basketTotal} displayType={'text'} decimalSeparator="." decimalScale={2} thousandSeparator={true}  fixedDecimalScale={true} prefix={'$'}/>
+                                            </span>
+                                        </h4>
+                                        <Button 
+                                            noIcon
+                                            // loading={Puto el que lo lea!!}
+                                            title='Check Out'
+                                            width='w-full'
+                                            onClick={createCheckoutSession}
+                                        />
                                     </div>
                                 </div>
                             </div>
