421dce7e7f5e7c8f363f8ba9d7b055348c758472
svei00 <79055634+svei00@users.noreply.github.com>
2022-11-13 20:16:47 -0800

    Working on under cunstruction page

--- components/Under.tsx
+++ components/Under.tsx
@@ -4,10 +4,10 @@ const Under = () => {
   return (
     <div className='underConstruction'>
         <div className='container'>
-
+            <h1>Under Construction</h1>
         </div>
     </div>
-  )
+  );
 }
 
 export default Under
---
+++ pages/history.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Under from '../components/Under'
+
+const History = () => {
+  return (
+    <div>
+        <Header />
+        <Under />
+    </div>
+  )
+  
+}
+
+export default History
