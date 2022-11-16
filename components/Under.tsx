69e111443b090a7b47052595d39d7cee1429bf96
svei00 <79055634+svei00@users.noreply.github.com>
2022-11-15 19:45:14 -0800

    Working on under construction page

--- components/Under.tsx
+++ components/Under.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import dog from '../assets/images/dog-eat.png';
 
 const Under = () => {
   return (
     <div className='underConstruction'>
-        <div className='container'>
+        <div className='underContainer'>
             <h1>Under Construction</h1>
+            <Image 
+                src={dog} 
+                alt = 'Dog under Construction'
+                layout = 'fill'
+                objectFit='contain'
+            />
         </div>
     </div>
   );
