4bbcc197462c4378abd8f1d811062b17446870b8
svei00 <79055634+svei00@users.noreply.github.com>
2022-11-16 19:35:53 -0800

    Keep working in uderconstruction page

--- components/Under.tsx
+++ components/Under.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
 import dog from '../assets/images/dog-eat.png';
+import Button from './Button';
 
 const Under = () => {
   return (
     <div className='underConstruction'>
         <div className='underContainer'>
-            <h1>Under Construction</h1>
-            <Image 
-                src={dog} 
-                alt = 'Dog under Construction'
-                layout = 'fill'
-                objectFit='contain'
-            />
+            <p>Under Construction</p>
+            <h1>We're Launching Soon!!</h1>
+            <Button title='Learn More'/>
+            <div className='underImage'>
+                <Image 
+                    src={dog}
+                    alt = 'Dog under Construction'
+                    layout = 'fill'
+                    objectFit='contain'
+                />
+            </div>
         </div>
     </div>
   );
