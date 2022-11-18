f18855da4cd606881addeab7d1706516c3f6ffdc
svei00 <79055634+svei00@users.noreply.github.com>
2022-11-17 20:08:31 -0800

    keep working on page underconstruction

--- components/Under.tsx
+++ components/Under.tsx
@@ -2,14 +2,39 @@ import React from 'react';
 import Image from 'next/image';
 import dog from '../assets/images/dog-eat.png';
 import Button from './Button';
+import { TbClick } from 'react-icons/tb';
 
 const Under = () => {
   return (
     <div className='underConstruction'>
         <div className='underContainer'>
             <p>Under Construction</p>
-            <h1>We're Launching Soon!!</h1>
-            <Button title='Learn More'/>
+            <h1>We're <span>Launching</span> Soon!!</h1>
+            
+            <div className='launchTime'>
+                <div>
+                    <p>00</p>
+                    <span>Days</span>
+                </div>
+                <div>
+                    <p>00</p>
+                    <span>Hours</span>
+                </div>
+                <div>
+                    <p>00</p>
+                    <span>Minutes</span>
+                </div>
+                <div>
+                    <p>00</p>
+                    <span>Seconds</span>
+                </div>
+            </div>
+            
+            <Button 
+                title={<span className='inline-flex'>Learn More  <TbClick /></span>}  
+                onClick={() => ''}
+            />
+            
             <div className='underImage'>
                 <Image 
                     src={dog}
