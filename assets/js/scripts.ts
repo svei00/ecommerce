20ed7d71a04306bc1bd42aba3d41f058d5b37306
svei00 <79055634+svei00@users.noreply.github.com>
2022-11-19 22:02:42 -0800

    working in the countdown

--- assets/js/scripts.ts
+++ assets/js/scripts.ts
@@ -1,3 +1,29 @@
 export const getCurrentYear = () => {
     return new Date().getFullYear();
   };
+
+  export function getRemainingTimeUntilMsTimeStamp(timeStampMs) {
+    
+    const timestampDayjs = dayjs(timeStampMs);
+    const nowDayjs = dayjs()
+    
+    return {
+      seconds: seconds,
+      minutes: minutes,
+      hours: hours,
+      days: days
+    };
+
+    const countDownDate = new Date('Jan 1, 2023 00:00:00').getTime();
+    const x = setInterval(function(){
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+  
+      const secs = Math.floor((distance / (1000 * 60 )) / 1000 );
+      const minutes = Math.floor((distance / (1000 * 60 * 60)) / (1000 * 60));
+      const hours = Math.floor((distance / (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  
+   }, 1000)
+
+  }
--- components/Under.tsx
+++ components/Under.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import dog from '../assets/images/boxer.jpg';
 import Button from './Button';
 import { TbClick } from 'react-icons/tb';
+import { getRemainingTimeUntilMsTimeStamp } from '../assets/js/scripts'
 
-const Under = () => {
+
+const defaulTime = {
+    seconds: '00',
+    minutes: '00',
+    hours: '00',
+    days: '00'
+}
+
+const Under = ({countdownTimeStampMS}) => {
+
+  const [reamainingTime, setRemainingTime] = useState(defaulTime);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+        updateRemainingTime({countdownTimeStampMS});
+    }, 1000);
+    return () => clearInterval(intervalId);
+  },[countdownTimeStampMS])
+
+  function updateRemainingTime(countdown) {
+    setRemainingTime(getRemainingTimeUntilMsTimeStamp(countdown))
+  }
+
   return (
     <div className='underConstruction'>
         <div className='underContainer'>
@@ -13,19 +36,19 @@ const Under = () => {
             
             <div className='launchTime'>
                 <div>
-                    <p>00</p>
+                    <p>{reamainingTime.days}</p>
                     <span>Days</span>
                 </div>
                 <div>
-                    <p>00</p>
+                    <p>{reamainingTime.hours}</p>
                     <span>Hours</span>
                 </div>
                 <div>
-                    <p>00</p>
+                    <p>{reamainingTime.minutes}</p>
                     <span>Minutes</span>
                 </div>
                 <div>
-                    <p>00</p>
+                    <p>{reamainingTime.seconds}</p>
                     <span>Seconds</span>
                 </div>
             </div>
