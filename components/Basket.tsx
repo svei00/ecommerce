bcba4bee71b5622e21a4fdbaf234d6aec9490093
svei00 <79055634+svei00@users.noreply.github.com>
2024-10-10 22:25:04 -0700

    Fixing the basket.tsx link issue revert the changtes

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -1,25 +1,25 @@
 import { ShoppingBagIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link'; // Next.js link
-import { useSelector } from 'react-redux';
-import { selectBasketItems } from '../redux/basketSlice';
+import Link from 'next/link';
+import { useSelector } from 'react-redux'
+import { selectBasketItems } from '../redux/basketSlice'
 
 function Basket() {
     const items = useSelector(selectBasketItems);
 
-    if (items.length === 0) return null;
+    if (items.length === 0 ) return null;
 
     return (
         <Link href='/checkout'>
-            <a className='shoppingCartOut'> {/* Wrap it in an anchor tag */}
+            <div className='shoppingCartOut'>
                 {items.length > 0 && (
                     <span className='shoppingCartIn'>
                         {items.length}
                     </span>
                 )}
-                <ShoppingBagIcon className='headerIcon h-8 w-8' />
-            </a>
+                <ShoppingBagIcon className='headerIcon h-8 w-8'/>
+            </div>    
         </Link>
     );
 }
 
-export default Basket;
+export default Basket
