de7fac104c65661bf65c1bb815b9ba55aade8a83
svei00 <79055634+svei00@users.noreply.github.com>
2024-10-18 20:25:33 -0700

    Deploying to Netlify v1

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -1,25 +1,28 @@
 import { ShoppingBagIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { useSelector } from 'react-redux'
-import { selectBasketItems } from '../redux/basketSlice'
+import { useSelector } from 'react-redux';
+import { selectBasketItems } from '../redux/basketSlice';
 
 function Basket() {
     const items = useSelector(selectBasketItems);
 
-    if (items.length === 0 ) return null;
+    if (items.length === 0) return null;
 
     return (
         <Link href='/checkout'>
-            <div className='shoppingCartOut'>
-                {items.length > 0 && (
-                    <span className='shoppingCartIn'>
-                        {items.length}
-                    </span>
-                )}
-                <ShoppingBagIcon className='headerIcon h-8 w-8'/>
-            </div>    
+            {/* Wrap the div with an anchor tag */}
+            <a>
+                <div className='shoppingCartOut'>
+                    {items.length > 0 && (
+                        <span className='shoppingCartIn'>
+                            {items.length}
+                        </span>
+                    )}
+                    <ShoppingBagIcon className='headerIcon h-8 w-8' />
+                </div>
+            </a>
         </Link>
     );
 }
 
-export default Basket
+export default Basket;
