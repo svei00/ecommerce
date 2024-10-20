5eaa2a10693d90b77091de60783819646dd47023
svei00 <79055634+svei00@users.noreply.github.com>
2024-10-20 14:52:44 -0700

    updating dependencies v3

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -9,15 +9,18 @@ function Basket() {
     if (items.length === 0) return null;
 
     return (
-        <Link href='/checkout'>
-            <div className='shoppingCartOut'>
-                {items.length > 0 && (
-                    <span className='shoppingCartIn'>
-                        {items.length}
-                    </span>
-                )}
-                <ShoppingBagIcon className='headerIcon h-8 w-8' />
-            </div>
+        <Link href="/checkout">
+            {/* Ensure there's an anchor element */}
+            <a>
+                <div className='shoppingCartOut'>
+                    {items.length > 0 && (
+                        <span className='shoppingCartIn'>
+                            {items.length}
+                        </span>
+                    )}
+                    <ShoppingBagIcon className='headerIcon h-8 w-8' />
+                </div>
+            </a>
         </Link>
     );
 }
