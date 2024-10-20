a4ffbad55819baab8d2e8711aee192ebcccedaaa
svei00 <79055634+svei00@users.noreply.github.com>
2024-10-20 15:10:30 -0700

    rever updating next.config.js export to false to avoid link restrictions and change Link tag

--- components/Basket.tsx
+++ components/Basket.tsx
@@ -1,24 +1,27 @@
+import { useRouter } from 'next/router'; // Import useRouter from next/router
 import { ShoppingBagIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import { selectBasketItems } from '../redux/basketSlice';
 
 function Basket() {
     const items = useSelector(selectBasketItems);
+    const router = useRouter(); // Initialize useRouter
 
     if (items.length === 0) return null;
 
+    const handleCheckout = () => {
+        router.push('/checkout'); // Programmatic navigation
+    };
+
     return (
-        <Link href="/checkout">
-            <div className='shoppingCartOut'>
-                {items.length > 0 && (
-                    <span className='shoppingCartIn'>
-                        {items.length}
-                    </span>
-                )}
-                <ShoppingBagIcon className='headerIcon h-8 w-8' />
-            </div>
-        </Link>
+        <div className='shoppingCartOut' onClick={handleCheckout} style={{ cursor: 'pointer' }}>
+            {items.length > 0 && (
+                <span className='shoppingCartIn'>
+                    {items.length}
+                </span>
+            )}
+            <ShoppingBagIcon className='headerIcon h-8 w-8' />
+        </div>
     );
 }
 
