c755ec73fba3fc635a778bf962de4d02e4d782e4
svei00 <79055634+svei00@users.noreply.github.com>
2024-10-29 21:22:14 -0700

    Fixing button jsx issue

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ interface Props {
     children?: React.ReactNode;
 }
 
-const Button = ({ title, onClick, width, loading, padding, noIcon, children }: Props): React.ReactElement => {
+const Button = ({ title, onClick, width, loading, padding, noIcon, children }: Props): React.ReactNode => {
     return (
         <button
             className={`ease group relative z-30 box-border inline-flex ${width ? width : 'w-auto'} ${padding} cursor-pointer items-center justify-center overflow-hidden rounded bg-gray-700 bg-gradient-to-r from-red-600 to-yellow-600 px-8 py-3 font-bold text-white transition-all duration-300 focus:outline-none`}
@@ -19,7 +19,7 @@ const Button = ({ title, onClick, width, loading, padding, noIcon, children }: P
             <span className="absolute bottom-0 right-0 -mb-8 -mr-5 h-20 w-8 translate-x-1 rotate-45 transform bg-white opacity-10 transition-all duration-300 ease-out group-hover:translate-x-0"></span>
 
             <span className="relative z-20 flex items-center font-semibold">
-                {noIcon && (
+                {!noIcon && (
                     <svg
                         className="relative mr-2 h-5 w-5 flex-shrink-0 text-white"
                         fill="none"
